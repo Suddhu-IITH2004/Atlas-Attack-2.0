1e78afd5a1adf1b69830173799403dc078a31d02
otsuka-suddhu-103974 <jajula.suddhodhan.a@otsuka.jp>
2026-06-05 14:11:52 +0900

    Env file created

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/Atlas-Attack-2.0/', // Add this line
   plugins: [
     react(),
     VitePWA({
