829bdc709a44475ae19914b9b74c46f3855071fd
Suddhodhan <105162371+Suddhu-IITH2004@users.noreply.github.com>
2026-06-05 13:29:13 +0900

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/Atlas-Attack-2.0/', // Add this line
   plugins: [
     react(),
     VitePWA({
