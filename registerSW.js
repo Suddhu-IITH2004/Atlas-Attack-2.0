2037fccc8be4ba674f767d63bb5edc5d657f947c
otsuka-suddhu-103974 <otsuka-suddhu-103974@users.noreply.github.com>
2026-06-05 05:14:19 +0000

    deploy: 1e78afd5a1adf1b69830173799403dc078a31d02

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Atlas-Attack-2.0/sw.js', { scope: '/Atlas-Attack-2.0/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
